b8ee342594c6f6483333ee6abafcbe75e7dc8af8
Vinod Dwarakanadha <vdwarakanadha@yodlee.com>
2019-12-03 16:46:21 +0530

    Updates

---
+++ precache-manifest.b4c81d335cde1ea6992d0cf60d1d90d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60eccc03647eb85cf911b5797860d672",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "fa1c9fb9be477baad4b6",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8072c64069df12c48bcc",
+    "url": "/my-app/static/js/2.95e7e4f0.chunk.js"
+  },
+  {
+    "revision": "fa1c9fb9be477baad4b6",
+    "url": "/my-app/static/js/main.c8090fe4.chunk.js"
+  },
+  {
+    "revision": "2067abcc1b1d9bcd4e07",
+    "url": "/my-app/static/js/runtime-main.98b5c931.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
